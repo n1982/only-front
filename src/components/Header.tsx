b5b092d4e818bf91fb864442180a32448e8ca5a2
nick selivanov <nick.selivanov@gmail.com>
2022-06-04 11:00:24 +0300

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 0;
+`;
+
+const HeaderTitle = styled.div`
+  font-size: 64px;
+  font-weight: 700;
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>ONLY.</HeaderTitle>
+    </HeaderContainer>
+  );
+};
+export default Header;
