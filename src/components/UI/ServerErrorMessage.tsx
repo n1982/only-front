b7f194d25a798e9ca5e8ee9139a460ef16d38f44
nick selivanov <nick.selivanov@gmail.com>
2022-06-07 07:09:36 +0300

    add ServerErrorMessage component

---
+++ src/components/UI/ServerErrorMessage.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import attentionSvg from '../assets/svg/attention.svg';
+
+interface ServerErrorMessageProps {}
+
+const StyledMessage = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  width: 640px;
+  height: 60px;
+  padding: 0 20px;
+  margin-bottom: 30px;
+  background: #f5e9e9;
+  border: 1px solid #e26f6f;
+  border-radius: 8px;
+  outline: none;
+`;
+const ErrorText = styled.div`
+  font-size: 20px;
+`;
+
+const ServerErrorMessage: React.FC<ServerErrorMessageProps> = () => {
+  return (
+    <StyledMessage>
+      <img src={attentionSvg} alt='attention' />
+      <ErrorText>Пользователя test.user@example.com не существует</ErrorText>
+    </StyledMessage>
+  );
+};
+
+export default ServerErrorMessage;
